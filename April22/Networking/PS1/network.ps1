908d6c52354a24a2d5e94862aa18d51ddd0ddb5d
shaikkhajaibrahim <qtkhajacloud@gmail.com>
2022-04-10 08:12:49 +0530

    Added other network and peering connection

--- April22/Networking/PS1/network.ps1
+++ April22/Networking/PS1/network.ps1
@@ -12,3 +12,27 @@ $ntier = New-AzVirtualNetwork -Name 'ntier' `
     -ResourceGroupName $resourceGroup.ResourceGroupName `
     -Location $resourceGroup.Location -AddressPrefix '192.168.0.0/16'  `
     -Subnet $webSubnet,$appSubnet,$dbSubnet,$mgmtSubnet
+
+
+
+# create a subnet config
+$web1Subnet = New-AzVirtualNetworkSubnetConfig -Name 'web' -AddressPrefix '172.16.0.0/24'
+$app1Subnet = New-AzVirtualNetworkSubnetConfig -Name 'app' -AddressPrefix '172.16.1.0/24'
+$db1Subnet = New-AzVirtualNetworkSubnetConfig -Name 'db' -AddressPrefix '172.16.2.0/24'
+$mgmt1Subnet = New-AzVirtualNetworkSubnetConfig -Name 'mgmt' -AddressPrefix '172.16.3.0/24'
+
+$ntier1 = New-AzVirtualNetwork -Name 'ntier1' `
+    -ResourceGroupName $resourceGroup.ResourceGroupName `
+    -Location $resourceGroup.Location -AddressPrefix '172.16.0.0/16'  `
+    -Subnet $web1Subnet,$app1Subnet,$db1Subnet,$mgmt1Subnet
+
+
+# Peering from ntier to ntier1
+$ntiertontier1peering = Add-AzVirtualNetworkPeering -Name 'ntiertontier1' -VirtualNetwork $ntier `
+    -RemoteVirtualNetworkId $ntier1.Id
+
+# Peering from ntier1 to ntier
+$ntier1tontierpeering = Add-AzVirtualNetworkPeering -Name 'ntier1tontier' -VirtualNetwork $ntier1 `
+    -RemoteVirtualNetworkId $ntier.Id
+
+
