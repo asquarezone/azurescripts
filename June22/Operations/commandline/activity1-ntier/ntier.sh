3c3222e674f00e868541e781d70f1eaaf992582e
shaikkhajaibrahim <qtkhajacloud@gmail.com>
2022-06-17 07:30:16 +0530

    Added if conditions

--- June22/Operations/commandline/activity1-ntier/ntier.sh
+++ June22/Operations/commandline/activity1-ntier/ntier.sh
@@ -8,8 +8,19 @@ webCidr='10.10.0.0/24'
 businessCidr='10.10.1.0/24'
 dataCidr='10.10.2.0/24'
 
+# This script will delete the resource group if it exists
+
+if [ $(az group exists --name $resourceGroup) = true ]; then
+    echo "Resourcegroup $resourceGroup in region $location already exists, so recreating"
+    az group delete --name $resourceGroup -y
+else
+    echo "Resourcegroup $resourceGroup in region $location does not exists, so creating"
+fi
+
 # Create a resource group
-#az group create --location $location --name $resourceGroup
+az group create --location $location --name $resourceGroup
+
+echo "Created resourcegroup $resourceGroup in region $location"
 
 # Create a virtual network
 az network vnet create --name $vnetName --resource-group $resourceGroup --address-prefixes $vnetCidr 
