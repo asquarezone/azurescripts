1f3fea07821d1e77ab56551d6f1b8198dc771052
shaikkhajaibrahim <qtkhajacloud@gmail.com>
2022-12-16 12:46:38 +0530

    Added bash script to tag all the resources in resource group workshop

---
+++ Workshops/Dec22/activity1/tagallresources.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+for id in $(az resource list --resource-group workshop --output tsv --query "[].id")
+do
+    echo "Resource id is $id"
+    az tag create --resource-id "$id" \
+        --tags "Environment=Dev" "Project=azurelearning" "Release=v1.1" "Team=qtazure"
+done
