4beca44b3003aabfcd289444105a307f97474a78
shaikkhajaibrahim <qtkhajacloud@gmail.com>
2023-03-19 13:14:19 +0530

    Addd changes

--- Mar23/Compute/Workshop/activity1.sh
+++ Mar23/Compute/Workshop/activity1.sh
@@ -2,6 +2,13 @@
 
 # Get the vm ids of the machines with tag Env and value Dev
 vmIds=$(az vm list --query "[?tags.Env=='Dev'].id" --output tsv)
-echo "vm ids are ${vmIds}"
 
-az vm deallocate --ids $vmIds
+
+if [[ -z "$vmIds" ]]; then
+  echo "No vms found with tag Env = Dev"
+elif [[ -n "$vmIds" ]]; then
+  echo "vm ids are ${vmIds}"
+  az vm deallocate --ids $vmIds
+fi
+
+# run the below statements when vmIds is not empty
