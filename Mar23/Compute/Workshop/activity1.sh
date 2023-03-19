80d07d9699d56891c28a820de310e21106e53304
shaikkhajaibrahim <qtkhajacloud@gmail.com>
2023-03-19 13:18:48 +0530

    Added chagnes

--- Mar23/Compute/Workshop/activity1.sh
+++ Mar23/Compute/Workshop/activity1.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+tag_name=$1
+tag_value=$2
+
 # Get the vm ids of the machines with tag Env and value Dev
-vmIds=$(az vm list --query "[?tags.Env=='Dev'].id" --output tsv)
+vmIds=$(az vm list --query "[?tags.$tag_name=='$tag_value'].id" --output tsv)
 
 
 if [[ -z "$vmIds" ]]; then
-  echo "No vms found with tag Env = Dev"
+  echo "No vms found with tag $tag_name = $tag_value"
 elif [[ -n "$vmIds" ]]; then
   echo "vm ids are ${vmIds}"
   az vm deallocate --ids $vmIds
