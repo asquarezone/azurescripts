7ccda14b20a03aa4bd912ffbaf3e6cd5ce995e17
shaikkhajaibrahim <qtkhajacloud@gmail.com>
2023-03-19 13:23:21 +0530

    Added changes

--- Mar23/Compute/Workshop/activity1.sh
+++ Mar23/Compute/Workshop/activity1.sh
@@ -3,6 +3,15 @@
 tag_name=$1
 tag_value=$2
 
+if [[ -z "$tag_name" ]]; then
+    tag_name='Env'
+fi
+
+if [[ -z "$tag_value" ]]; then
+    tag_name='Dev'
+fi
+
+
 # Get the vm ids of the machines with tag Env and value Dev
 vmIds=$(az vm list --query "[?tags.$tag_name=='$tag_value'].id" --output tsv)
 
